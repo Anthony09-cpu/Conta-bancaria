70364113a26470d309363eafe7998e98ee1703ad
Anthony09-cpu <alaide.barbosa191@gmail.com>
2024-07-13 23:56:46 -0300

    Java

---
+++ conta/src/Conta.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Conta {
+    public static void main(String[] args) throws Exception {
+
+        String nome;
+        int idade;
+        double cpf;
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Conhecer e importar a classe Scanner
+        // Exibir as mesagens para o nosso usuario
+        // Obter pelo scanner os valores digitados
+        // Exibir a mensagem conta criada
+
+        System.out.println("Bem vindo ao Banco SI");
+        System.out.println("Deseja criar uma conta conosco?");
+        System.out.println("1 -> Sim");
+        System.out.println("2 -> Não");
+        String scanf = scanner.nextLine();
+
+        if (scanf.equals(1)) {
+            System.out.println("Você escolheu criar uma conta!!");
+            nome = scanner.nextLine();
+
+            System.out.println("Vamos começar informando seu nome:");
+            idade = Integer.parseInt(scanner.nextLine());
+
+            System.out.println("E por fim, seu CPF:");
+            cpf = Double.parseDouble(scanner.nextLine());
+
+            System.out.println("Conta Criada com Sucesso!!");
+            System.out.println("Nome: " + nome);
+            System.out.println("Idade: " + idade);
+            System.out.println("CPF: " + cpf);
+
+        } else if (scanf.equals("2")) {
+            System.out.println("Você não escolheu criar a conta!");
+            System.out.println("Obrigado pelo contato, volte sempre!!");
+        } else {
+            System.out.println("Opção Invalida");
+        }
+        scanner.close();
+    }
+}
